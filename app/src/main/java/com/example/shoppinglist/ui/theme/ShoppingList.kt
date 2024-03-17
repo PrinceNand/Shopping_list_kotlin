6d1ae21737af87d1f8243696047404f0b2de817c
Prince <ashnand2000@gmail.com>
2024-03-17 20:53:50 +0530

    Add the Alert Dialog to enter the value and functionality for cancel button

--- app/src/main/java/com/example/shoppinglist/ui/theme/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ui/theme/ShoppingList.kt
@@ -3,9 +3,15 @@ package com.example.shoppinglist.ui.theme
 import android.graphics.Paint.Align
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,24 +20,72 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ShoppingList(){
 
-    data class ShoppingItems(val id:Int, val name:String, val quantity:Int, val isEditing:Boolean = false)
+    data class ShoppingItems(val id:Int, var name:String, var quantity:Int, var isEditing:Boolean = false)
 
     var sItems by remember { mutableStateOf(listOf<ShoppingItems>()) }
+    var showDialog by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQuantity by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
+        Button(onClick = { showDialog=true }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
             Text(text = "Add Items!")
         }
 
         LazyColumn(modifier = Modifier.fillMaxSize()) {
-            item { sItems }
+            items(sItems){
+
+            }
         }
     }
+
+    if (showDialog){
+        AlertDialog(onDismissRequest = { showDialog=false },
+            confirmButton = {
+                // add the button Add / Cancel
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    // Add Button
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Add Item")
+                    }
+
+                    // Cancel Button
+                    Button(onClick = { showDialog = false }) {
+                        Text(text = "Cancel")
+                    }
+                } }, title = { Text(
+            text = "Add Shopping Items")},
+             text = {
+                 Column {
+                     // itemName
+                     OutlinedTextField(value = itemName, onValueChange ={
+                         itemName = it
+                     }, singleLine = true,
+                         modifier = Modifier
+                             .fillMaxWidth()
+                             .padding(8.dp))
+
+                     // itemQuantity
+                     OutlinedTextField(value = itemQuantity, onValueChange ={
+                         itemQuantity = it
+                     }, singleLine = true,
+                         modifier = Modifier
+                             .fillMaxWidth()
+                             .padding(8.dp))
+
+                 }
+             })
+    }
 }
+
